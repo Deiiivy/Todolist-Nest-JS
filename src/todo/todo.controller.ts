b195d5d18204bd24b2ac54675040ee0918d6c937
Deiiivy <deivypr28@gmail.com>
2026-02-23 20:20:16 -0500

    fix method getAllTasks in controller todo

--- src/todo/todo.controller.ts
+++ src/todo/todo.controller.ts
@@ -11,7 +11,7 @@ export class TodoController {
         return this.todoService.create(createTaskDto);
     }
 
-    @Get()
+    @Get(':userId')
     getAllTasks(@Param('userId') userId: string) {
         return this.todoService.findAll(userId);
     }
